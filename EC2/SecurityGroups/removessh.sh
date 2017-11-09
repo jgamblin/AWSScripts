ad892b38aee864fd587c93df02dc31dad03e4ab2
Jerry Gamblin <jerry.gamblin@gmail.com>
2017-11-09 11:15:57 -0600

    Adding SSH Rule Script
    Addign SSH Rule Script

---
+++ EC2/SecurityGroups/removessh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Settings
+OpenPort="22"
+IP=$(curl -s http://checkip.amazonaws.com)
+security_groups=$(aws ec2 describe-security-groups --query "SecurityGroups[].[GroupId, GroupName]" --output text | awk '{print $2;}')
+
+for security_group in $security_groups
+do
+  printf "Removing access from %s to port %s in the %s security group." "$IP" "$OpenPort" "$security_group"
+  printf "\n"
+  aws ec2 revoke-security-group-ingress --group-name "$security_group" --ip-permissions '[{"IpProtocol": "tcp", "FromPort": '$OpenPort', "ToPort": '$OpenPort', "IpRanges": [{"CidrIp": "'"$IP"'/32"}]}]'
+done
